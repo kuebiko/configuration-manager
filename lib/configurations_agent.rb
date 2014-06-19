17948a1301fbc77ffce5bc2094db83d8dbc4c0ea
David Ramalho <dramalho@gmail.com>
2014-06-19 19:24:03 +0100

    Send data without root node

--- lib/configurations_agent.rb
+++ lib/configurations_agent.rb
@@ -14,17 +14,23 @@ class ConfigurationsAgent < Kuebiko::Agent
   end
 
   def handle_query(msg)
-    @resources[msg.payload.query.downcase] ||= YAML.load_file("config_files/#{msg.payload.query.downcase}.yml")
+    @resources[msg.payload.query.downcase] ||= YAML.load_file(path_for_resource(msg.payload.query)).fetch('settings')
 
     if @resources[msg.payload.query.downcase]
       reply_msg = msg.build_reply_message
 
-      reply_msg.payload = Kuebiko::MessagePayload::Generic.new(body: @resources[msg.payload.query.downcase].to_s)
+      reply_msg.payload = Kuebiko::MessagePayload::Generic.new(body: @resources[msg.payload.query.downcase].to_json)
 
       dispatcher.send(reply_msg)
     end
   rescue StandardError => e
     puts "#{self.class.name}: #{e.message}"
     puts e.backtrace.inspect
+  end
+
+  protected
+
+  def path_for_resource(resource)
+    "config_files/#{resource.downcase}.yml"
   end
 end
